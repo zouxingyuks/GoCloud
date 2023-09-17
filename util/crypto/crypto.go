89e90d0800ad13ebb0400bd1a5c430beed79332f
ZouXingyu <zouxingyu@mail.ynu.edu.cn>
2023-09-17 23:05:41 +0800

    修正 crypto 接口

--- util/crypto/crypto.go
+++ util/crypto/crypto.go
@@ -1,4 +1,4 @@
-package util
+package crypto
 
 const (
 	PasswordCrypto = iota
@@ -16,16 +16,4 @@ func NewCrypto(object int) Crypto {
 	default:
 		return nil
 	}
-}
-
-type password struct {
-}
-
-func (password) Encrypt(plaintext string) (ciphertext string) {
-	//todo 密码的加密算法
-	return plaintext
-}
-func (password) Decrypt(ciphertext string) (plaintext string) {
-	//todo 密码的解密算法
-	return plaintext
 }
