4ad298d5919a92d4faabd77dd33bb4d565b0fd4d
ZouXingyu <zouxingyu@mail.ynu.edu.cn>
2023-09-17 23:14:09 +0800

    补充错误返回

--- util/crypto/crypto.go
+++ util/crypto/crypto.go
@@ -7,8 +7,8 @@ const (
 // Crypto
 // 选用 []byte 主要是 此处无需进行字符串比较,且字符串操作较多，使用 []byte 效率更高
 type Crypto interface {
-	Encrypt(plaintext []byte) (ciphertext string)
-	Decrypt(ciphertext []byte) (plaintext string)
+	Encrypt(plaintext []byte) (ciphertext string, err error)
+	Decrypt(ciphertext []byte) (plaintext string, err error)
 }
 
 func NewCrypto(object int) Crypto {
