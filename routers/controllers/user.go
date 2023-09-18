dc6784b37031058f8febb0b8c00a4ea80968f0d8
ZouXingyu <zouxingyu@mail.ynu.edu.cn>
2023-09-19 00:14:14 +0800

    用户注册接口，具体实现

---
+++ routers/controllers/user.go
@@ -0,0 +1,30 @@
+package controllers
+
+import (
+	"GoCloud/pkg/serializer"
+	user2 "GoCloud/service/user"
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+// userRegister 用户注册接口
+func userRegister(c *gin.Context) {
+	user := user2.NewUserService()
+	//todo 在参数设置这里尝试进行优化
+
+	res := serializer.Response{}
+
+	// 解析并验证用户注册信息
+	if err := c.ShouldBindJSON(&user); err == nil {
+		// 注册用户
+		res = user.Register()
+	} else {
+		// 参数错误
+		res = serializer.Response{
+			Code: http.StatusBadRequest,
+			Msg:  "参数错误",
+		}
+	}
+
+	c.JSON(res.Code, res)
+}
