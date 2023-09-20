5b062cbc7e7d82892007118f2bd4c71bee15eb71
ZouXingyu <zouxingyu@mail.ynu.edu.cn>
2023-09-20 15:08:52 +0800

    model.go 参数解析微调

--- service/user/model.go
+++ service/user/model.go
@@ -3,5 +3,5 @@ package user
 // Param 接口绑定参数
 type Param struct {
 	Email    string `form:"email" json:"email" binding:"required,email"`
-	Password string `form:"Password" json:"Password" binding:"required,min=4,max=64"`
+	Password string `form:"password" json:"password" binding:"required,min=4,max=64"`
 }
