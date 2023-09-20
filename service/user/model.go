11abc00cf4b927f4ad0cf6247a3bc301ef6fa71c
ZouXingyu <zouxingyu@mail.ynu.edu.cn>
2023-09-20 20:14:59 +0800

    json 解析成功

--- service/user/model.go
+++ service/user/model.go
@@ -2,6 +2,6 @@ package user
 
 // Param 接口绑定参数
 type Param struct {
-	Email    string `form:"email" json:"email" binding:"required,email"`
-	Password string `form:"password" json:"password" binding:"required,min=4,max=64"`
+	Email    string `json:"Email"  binding:"required"`
+	Password string `json:"Password"  binding:"required"`
 }
