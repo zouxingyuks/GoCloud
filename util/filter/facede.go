9fbeec6c6052c2723e9ed1205b62f02bcc978fee
ZouXingyu <zouxingyu@mail.ynu.edu.cn>
2023-09-21 23:43:11 +0800

    使用门面模式进行封装

---
+++ util/filter/facede.go
@@ -0,0 +1,6 @@
+package filter
+
+// filterFacade 门面模式的结构体，用于封装多种过滤操作
+type filterFacade struct{}
+
+var Facade filterFacade
--- util/filter/user.go
+++ util/filter/user.go
@@ -2,11 +2,6 @@ package filter
 
 import "net/mail"
 
-// filterFacade 门面模式的结构体，用于封装多种过滤操作
-type filterFacade struct{}
-
-var FilterFacade filterFacade
-
 // IsValidEmail 检查给定的电子邮件地址是否合法
 func (f *filterFacade) IsValidEmail(email string) bool {
 	_, err := mail.ParseAddress(email)
