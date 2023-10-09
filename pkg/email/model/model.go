4eec5c260ba39a64707f0917d20dd77b5569deab
ZouXingyu <zouxingyu@mail.ynu.edu.cn>
2023-10-09 08:47:06 +0800

    迁移模型位置尝试性将归属结构体放置到子包中

---
+++ pkg/email/model/model.go
@@ -0,0 +1,32 @@
+package model
+
+import "sync"
+
+type Mail struct {
+	Type string
+	Once sync.Once `mapstructure:"-"`
+	//同时最大发送数量
+	Size int
+	Smtp SMTP
+}
+
+type SMTP struct {
+	Host string
+	Once sync.Once `mapstructure:"-"`
+	//端口
+	Port int
+	//用户名
+	User string
+	//密码
+	Password string
+	//发送者名
+	Name string
+	//发送者地址
+	Address string
+	//回复地址
+	ReplyTo string
+	//是否启用加密
+	Encryption bool
+	//SMTP 连接保留时长
+	Keepalive int
+}
