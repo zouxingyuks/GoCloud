e989f63c1af6a6b3dd41dfab174dfa70519b6429
ZouXingyu <zouxingyu@mail.ynu.edu.cn>
2023-10-09 09:35:30 +0800

    补充 smtp 的配置初始值

---
+++ pkg/conf/default.go
@@ -0,0 +1,50 @@
+package conf
+
+import "GoCloud/pkg/email/model"
+
+var defaultConfig = map[string]interface{}{
+	"system": system{
+		Mode:          "master",
+		Debug:         false,
+		SessionSecret: "",
+	},
+	"database": database{
+		Type:        "UNSET",
+		User:        "",
+		Password:    "",
+		Host:        "",
+		Name:        "",
+		TablePrefix: "",
+		DBFile:      "cloudreve.db",
+		Port:        3306,
+		Charset:     "utf8",
+		UnixSocket:  false,
+	},
+	"redis": redis{
+		Network:  "tcp",
+		Server:   "",
+		User:     "",
+		Password: "",
+		DB:       "0",
+	},
+	"cors": cors{
+		AllowOrigins:     []string{"UNSET"},
+		AllowMethods:     []string{"PUT", "POST", "GET", "OPTIONS"},
+		AllowHeaders:     []string{"Cookie", "X-Cr-Policy", "Authorization", "Content-Length", "Content-Type", "X-Cr-Path", "X-Cr-FileName"},
+		AllowCredentials: false,
+		ExposeHeaders:    nil,
+		SameSite:         "Default",
+		Secure:           true,
+	},
+	"mail": model.Mail{
+		Type: 0,
+		Smtp: model.SMTP{
+			Host:     "smtp.example.com",
+			Port:     465,
+			User:     "",
+			Password: "",
+			Name:     "",
+			Address:  "",
+		},
+	},
+}
