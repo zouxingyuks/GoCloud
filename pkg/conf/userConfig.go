cc0bda3fa156bae0f79202bb96e71ea3ee841720
ZouXingyu <zouxingyu@mail.ynu.edu.cn>
2023-10-18 19:14:34 +0800

    补充系统信息的日志记录

--- pkg/conf/userConfig.go
+++ pkg/conf/userConfig.go
@@ -1,18 +1,25 @@
 package conf
 
-import "sync"
+import (
+	"log"
+	"sync"
+)
 
-type user struct {
+// todo 默认群组的管理
+type userController struct {
 	DefaultGroup int
+	EmailVerify  bool
 	once         sync.Once
 }
 
-var userConfig = new(user)
+var userConfig = new(userController)
 
 // UserConfig 系统公用静态配置
-func UserConfig() *user {
+func UserConfig() *userController {
 	userConfig.once.Do(func() {
-		Config().Unmarshal(&userConfig)
+		log.Println("init UserConfig...")
+		Config().Sub("usercontroller").Unmarshal(&userConfig)
+		log.Println("init UserConfig success")
 	})
 	return userConfig
 }
