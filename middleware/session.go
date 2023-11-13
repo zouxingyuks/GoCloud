34c04099d4b50064160ceccd882fd6182d215e20
ZouXingyu <zouxingyu@mail.ynu.edu.cn>
2023-11-13 15:06:27 +0800

    session 的中间件 主要是将密码的更改锁定为只有系统启动时才能生效

--- middleware/session.go
+++ middleware/session.go
@@ -1,41 +1,59 @@
 package middleware
 
 import (
+	"GoCloud/conf"
 	"github.com/gin-contrib/sessions"
 	"github.com/gin-contrib/sessions/cookie"
+	"github.com/gin-contrib/sessions/memstore"
+	"github.com/gin-contrib/sessions/redis"
 	"github.com/gin-gonic/gin"
+	"net/http"
+	"strings"
 )
 
-//// Store session存储
-//var Store sessions.Store
+var secret = []byte(conf.StaticConfig().Session.Secret)
 
 // Session 初始化session
-func Session(secret string) gin.HandlerFunc {
-	//todo 修改 store
-	store := cookie.NewStore([]byte("your-secret-key"))
-	// Redis设置不为空，且非测试模式时使用Redis
-	//Store = sessionstore.NewStore(cache.Store, []byte(secret))
-	//
-	//sameSiteMode := http.SameSiteDefaultMode
-	//switch strings.ToLower(conf.CORSConfig().SameSite) {
-	//case "default":
-	//	sameSiteMode = http.SameSiteDefaultMode
-	//case "none":
-	//	sameSiteMode = http.SameSiteNoneMode
-	//case "strict":
-	//	sameSiteMode = http.SameSiteStrictMode
-	//case "lax":
-	//	sameSiteMode = http.SameSiteLaxMode
-	//}
-	//
+func Session() gin.HandlerFunc {
+	var err error
+	store := sessions.Store(nil)
+	switch conf.StaticConfig().Session.Store {
+	case "redis":
+		store, err = redis.NewStore(conf.RedisConfig().PoolSize, conf.RedisConfig().Network, conf.RedisConfig().Server, conf.RedisConfig().Password, secret)
+		if err != nil {
+			panic(err)
+		}
+	case "cookie":
+		store = cookie.NewStore(secret)
+
+	case "memory":
+		store = memstore.NewStore(secret)
+
+	}
+
+	sameSiteMode := http.SameSiteDefaultMode
+	switch strings.ToLower(conf.CORSConfig().SameSite) {
+	case "default":
+		sameSiteMode = http.SameSiteDefaultMode
+	case "none":
+		sameSiteMode = http.SameSiteNoneMode
+	case "strict":
+		sameSiteMode = http.SameSiteStrictMode
+	case "lax":
+		sameSiteMode = http.SameSiteLaxMode
+	default:
+		sameSiteMode = http.SameSiteDefaultMode
+	}
+
 	//// Also set Secure: true if using SSL, you should though
-	//Store.Options(sessions.Options{
-	//	HttpOnly: true,
-	//	MaxAge:   60 * 86400,
-	//	Path:     "/",
-	//	SameSite: sameSiteMode,
-	//	Secure:   conf.CORSConfig().Secure,
-	//})
+	store.Options(sessions.Options{
+		HttpOnly: conf.SystemConfig().Sessions.Option.HttpOnly,
+		//86400 是一天
+		MaxAge:   conf.SystemConfig().Sessions.Option.MaxAge,
+		Path:     "/",
+		SameSite: sameSiteMode,
+		Secure:   conf.SystemConfig().Sessions.Option.Secure,
+	})
 
 	return sessions.Sessions("gocloud-session", store)
 }
