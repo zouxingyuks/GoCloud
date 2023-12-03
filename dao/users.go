d480081ac30c1dea41fd92414358ea6a5268e0db
ZouXingyu <zouxingyu@mail.ynu.edu.cn>
2023-12-03 19:40:02 +0800

    补充刷新 user 信息的逻辑

--- dao/users.go
+++ dao/users.go
@@ -46,6 +46,41 @@ func storeUserInCache(u *User) (err error) {
 	return nil
 }
 
+// updateUserInCache 更新缓存中的用户信息
+// 根据 uuid 定位旧的 User ，然后将新的 User 的信息更新到旧的 User 中，只更新非空字段
+func updateUserInCache(uuid string, newUser *User) (err error) {
+	//预防缓存雪崩，设置随机过期时间
+	ttl := cache.Second(600 + rand.Intn(600)) // 10min~20min
+	data, err := util.StructToMapF1(newUser)
+	delete(data, "uuid")
+	for k, v := range data {
+		if v == "" {
+			delete(data, k)
+		}
+	}
+	//更新
+	err = Cache().HMSet("uuid:"+uuid, data, ttl)
+	if err != nil {
+		return errors.Wrap(err, "set cache error")
+	}
+	return nil
+
+}
+
+// refreshUserInCache 刷新缓存中的用户信息
+func refreshUserInCache(uuid string) (u *User, err error) {
+	u = new(User)
+	err = DB().Where("uuid = ?", uuid).First(u).Error
+	if err != nil {
+		return u, errors.Wrap(err, "get user from db error")
+	}
+	err = storeUserInCache(u)
+	if err != nil {
+		return nil, errors.Wrap(err, "set cache error")
+	}
+	return u, nil
+}
+
 // GetUserByEmail 根据邮箱获取用户信息
 func GetUserByEmail(email string) (u *User, err error) {
 	u = new(User)
--- dao/users_test.go
+++ dao/users_test.go
@@ -4,13 +4,11 @@ import (
 	"GoCloud/conf"
 	"GoCloud/pkg/log"
 	"testing"
-	"time"
 )
 
 func TestGetUserByEmail(t *testing.T) {
 	conf.AddPath("..")
 	log.SetFilepath("../log/zap.log")
-	time.Sleep(time.Second * 5)
 	u, err := GetUserByEmail("1308345487@qq.com")
 	if err != nil {
 		t.Fatal(err)
@@ -21,7 +19,6 @@ func TestGetUserByEmail(t *testing.T) {
 func Test_findUserInCache(t *testing.T) {
 	conf.AddPath("..")
 	log.SetFilepath("../log/zap.log")
-	time.Sleep(time.Second * 5)
 	u, err := findUserInCache("8ae508bc-1e19-5795-93c9-860440c393ac")
 	if err != nil {
 		t.Fatal(err)
@@ -32,8 +29,28 @@ func Test_findUserInCache(t *testing.T) {
 func TestGetUserByUUID(t *testing.T) {
 	conf.AddPath("..")
 	log.SetFilepath("../log/zap.log")
-	time.Sleep(time.Second * 5)
 	u, err := GetUserByUUID("8ae508bc-1e19-5795-93c9-860440c393ac")
+	if err != nil {
+		t.Fatal(err)
+	}
+	t.Log(u)
+}
+
+func Test_updateUserInCache(t *testing.T) {
+	conf.AddPath("..")
+	log.SetFilepath("../log/zap.log")
+	err := updateUserInCache("1182bd1f-9d5d-55f9-bb2f-7104adfbb513", &User{
+		NickName: "test",
+	})
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+func Test_refreshUserInCache(t *testing.T) {
+	conf.AddPath("..")
+	log.SetFilepath("../log/zap.log")
+	u, err := refreshUserInCache("1182bd1f-9d5d-55f9-bb2f-7104adfbb513")
 	if err != nil {
 		t.Fatal(err)
 	}
