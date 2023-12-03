5c76680b4dc408521b9a1314a7119f6bc3d94341
ZouXingyu <zouxingyu@mail.ynu.edu.cn>
2023-12-03 19:05:40 +0800

    修正缓存为命中的情况

--- dao/users.go
+++ dao/users.go
@@ -8,6 +8,41 @@ import (
 	"math/rand"
 )
 
+// 从缓存中查询用户信息
+func findInCache(uuid string) (u *User, err error) {
+	u = new(User)
+	data, err := Cache().HGetAll("uuid:" + uuid)
+	if err != nil {
+		return nil, errors.Wrap(err, "get user from cache error")
+	}
+	if len(data) == 0 {
+		return nil, errors.New("user not found")
+	}
+	err = mapstructure.Decode(data, u)
+	if err != nil {
+		return nil, errors.Wrap(err, "mapstructure decode error")
+	}
+	return u, nil
+}
+
+// storeInCache 将用户信息存入缓存
+func storeInCache(u *User) (err error) {
+	//预防缓存雪崩，设置随机过期时间
+	ttl := cache.Second(600 + rand.Intn(600)) // 10min~20min
+	// email 与 uuid 的映射
+	Cache().Set("email:"+u.Email, u.UUID, ttl)
+	data, err := util.StructToMapF1(u)
+	if err != nil {
+		return errors.Wrap(err, "struct to map error")
+	}
+	// uuid 与 user 的映射
+	err = Cache().HMSet("uuid:"+u.UUID, data, ttl)
+	if err != nil {
+		return errors.Wrap(err, "set cache error")
+	}
+	return nil
+}
+
 // GetUserByEmail 根据邮箱获取用户信息
 func GetUserByEmail(email string) (u *User, err error) {
 	u = new(User)
@@ -19,30 +54,36 @@ func GetUserByEmail(email string) (u *User, err error) {
 	}
 	//2. 如果缓存中没有，就去数据库中查找
 	u = new(User)
-	DB().Where("email = ?", email).First(u)
-	Cache().Set("email:"+email, u.UUID, cache.Second(600+rand.Intn(600)))
-	data, err := util.StructToMapF1(u)
+	err = DB().Where("email = ?", email).First(u).Error
 	if err != nil {
-		return nil, errors.Wrap(err, "struct to map error")
+		return u, errors.Wrap(err, "get user from db error")
 	}
-	//预防缓存雪崩，设置随机过期时间
-	err = Cache().HMSet("uuid:"+u.UUID, data, cache.Second(600+rand.Intn(600)))
+	err = storeInCache(u)
 	if err != nil {
 		return nil, errors.Wrap(err, "set cache error")
 	}
 	return u, nil
 }
 
-// 从缓存中查询用户信息
-func findInCache(uuid string) (u *User, err error) {
-	u = new(User)
-	data, err := Cache().HGetAll("uuid:" + uuid)
-	if err != nil {
-		return nil, errors.Wrap(err, "get user from cache error")
+// GetUserByUUID 根据UUID获取用户信息
+func GetUserByUUID(uuid string) (*User, error) {
+	if uuid == "" {
+		return nil, errors.New("uuid is empty")
 	}
-	err = mapstructure.Decode(data, u)
+	//先在缓存中查找
+	u, err := findInCache(uuid)
+	if err == nil && u != nil {
+		return u, nil
+	}
+	//如果缓存中没有再去数据库中查找
+	u = new(User)
+	err = DB().Where("uuid = ?", uuid).First(u).Error
 	if err != nil {
-		return nil, errors.Wrap(err, "mapstructure decode error")
+		return u, errors.Wrap(err, "get user from db error")
+	}
+	err = storeInCache(u)
+	if err != nil {
+		return nil, errors.Wrap(err, "set cache error")
 	}
 	return u, nil
 }
