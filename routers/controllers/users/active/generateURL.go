ac7dc43811ae1d8d3d9378ebdc46f925e8c4871e
ZouXingyu <zouxingyu@mail.ynu.edu.cn>
2023-12-20 08:58:24 +0800

    生成激活链接

---
+++ routers/controllers/users/active/generateURL.go
@@ -0,0 +1,27 @@
+package active
+
+import (
+	"GoCloud/conf"
+	token2 "GoCloud/service/token"
+	"github.com/pkg/errors"
+	"net/url"
+)
+
+func generateURL(uuid string) (string, error) {
+	// 生成激活链接
+	if token, err := token2.Generate(activeAction, uuid, exp); err != nil {
+		return "", errors.Wrap(err, "Error generating activation token")
+	} else {
+		u := url.URL{}
+		u.Path, err = url.JoinPath("/users/activate/" + token)
+		if err != nil {
+			return "", errors.Wrap(err, "Error generating activation link")
+		}
+		u.Scheme = "http"
+		u.Host = conf.SiteConfig().Domain
+		if conf.SiteConfig().SSL {
+			u.Scheme = "https"
+		}
+		return u.String(), nil
+	}
+}
